e9044976857ba56a19bb19f1c82f557f0ab0e62d
Rodrigo Passos <rodrigo.passos25@gmail.com>
2021-06-23 00:31:06 -0300

    :sparkles: Adicionado autenticação e estrutura da home

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_model.dart';
 
 class LoginController {
-  Future<void> googleSignIn() async {
+  final authController = AuthController();
+  Future<void> googleSignIn(BuildContext context) async {
     GoogleSignIn _googleSignIn = GoogleSignIn(
       scopes: [
         'email',
@@ -10,9 +14,14 @@ class LoginController {
 
     try {
       final response = await _googleSignIn.signIn();
-      print(response);
+      final user = UserModel(
+        name: response!.displayName!,
+        photoURL: response.photoUrl,
+      );
+
+      authController.setUser(context, user);
     } catch (error) {
-      print(error);
+      authController.setUser(context, null);
     }
   }
 }
